890e8eab9655b9ed0b81ea2ac94cac3a5e8fc412
HuynhLongNhat <nhathuynh227@gmail.com>
2023-11-27 22:46:48 +0700

    video 55

--- src/services/userService.js
+++ src/services/userService.js
@@ -23,7 +23,7 @@ let handleUserLogin = (email, password) => {
       if (isExist) {
         //user already exist
         let user = await db.User.findOne({
-          attributes: ["email", "roleId", "password"],
+          attributes: ["email", "roleId", "password", "firstName", "lastName"],
           where: { email: email },
           raw: true,
         });
@@ -109,23 +109,23 @@ let createNewUser = (data) => {
           errCode: 1,
           errMessage: "Your email is already is used , Plz try another email!",
         });
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassWord(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          gender: data.gender === "1" ? true : false,
+          roleId: data.roleId,
+          phoneNumber: data.phoneNumber,
+        });
+        resolve({
+          errCode: 0,
+          errMessage: "Ok",
+        });
       }
-
-      let hashPasswordFromBcrypt = await hashUserPassWord(data.password);
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        gender: data.gender === "1" ? true : false,
-        roleId: data.roleId,
-        phoneNumber: data.phoneNumber,
-      });
-      resolve({
-        errCode: 0,
-        errMessage: "Ok",
-      });
     } catch (error) {
       reject(e);
     }
@@ -141,10 +141,15 @@ let deleteUser = (userId) => {
       if (!user) {
         resolve({
           errCode: 2,
-          errMessage: `The user isn't  exist`,
+          errMessage: `The user isn't exist`,
         });
       }
-      await user.destroy();
+
+      await db.User.destroy({
+        where: {
+          id: userId,
+        },
+      });
       resolve({
         errCode: 0,
         message: `The user is deleted`,
@@ -191,10 +196,34 @@ let updateUserData = (data) => {
     }
   });
 };
+
+let getAllCodeService = (typeInput) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!typeInput) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameters !",
+        });
+      } else {
+        let res = {};
+        let allcode = await db.Allcode.findAll({
+          where: { type: typeInput },
+        });
+        res.errCode = 0;
+        res.data = allcode;
+        resolve(res);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 module.exports = {
   handleUserLogin: handleUserLogin,
   getAllUsers: getAllUsers,
   createNewUser: createNewUser,
   deleteUser: deleteUser,
   updateUserData: updateUserData,
+  getAllCodeService: getAllCodeService,
 };
