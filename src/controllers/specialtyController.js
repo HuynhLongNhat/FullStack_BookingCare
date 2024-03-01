26a55c9552a12b9252b09f4f8395678376f689bd
HuynhLongNhat <nhathuynh227@gmail.com>
2024-03-01 14:36:06 +0700

    video 92

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -1,7 +1,6 @@
 import specialtyService from "../services/specialtyService";
 
 let createSpecialty = async (req, res) => {
-  console.log("data specialty  :", req.body);
   try {
     let infor = await specialtyService.createSpecialty(req.body);
     return res.status(200).json(infor);
@@ -14,6 +13,19 @@ let createSpecialty = async (req, res) => {
   }
 };
 
+let getAllSpecialty = async (req, res) => {
+  try {
+    let infor = await specialtyService.getAllSpecialty();
+    return res.status(200).json(infor);
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server ",
+    });
+  }
+};
 module.exports = {
   createSpecialty: createSpecialty,
+  getAllSpecialty,
 };
